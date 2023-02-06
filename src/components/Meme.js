6c3eb1ab7d8f8cab6c187287d6f93705f457d1e8
Omar <omsa04@gmail.com>
2023-02-06 12:18:34 -0700

    created meme js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,21 @@
 import React from "react";
-
+import memeData from "./memeData.js";
 export default function Meme() {
+  function handleClick() {
+    let arr = memeData.data.memes;
+    let item = arr[Math.floor(Math.random() * arr.length)];
+    console.log(item.url);
+  }
+
   return (
     <div className="formsection">
-      <form className="form">
+      <div className="form">
         <input type="text" className="fromInput" placeholder="Top Text" />
-        <input type="text" className="fromInput" placeholder="Bottom Text"/>
-        <button className="formButton">Get a new meme image 🖼</button>
-      </form>
+        <input type="text" className="fromInput" placeholder="Bottom Text" />
+        <button onClick={handleClick} className="formButton">
+          Get a new meme image 🖼
+        </button>
+      </div>
     </div>
   );
 }
